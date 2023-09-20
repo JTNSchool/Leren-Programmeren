f8bdebf5c3018753324ac6ba01ab41a94ca472a6
Jacco Bos <99077364@mydavinci.nl>
2023-09-20 12:05:35 +0200

    metenisweten toegevoegt

---
+++ Module_1/Deel 3 Contitions & Forks/MetenIsWeten.py
@@ -0,0 +1,27 @@
+# Maak een programma dat 2 gehele getallen opvraagt aan de gebruiker. Stel die getallen zijn toegekend aan de variabelen a en b
+# Bepaal (met een if-statement) of a groter is dan b
+# Zo ja:
+# Ken de waarde van a toe aan een variabele Max
+# Laat het programma printen: ‘a is het grootste getal: ’ gevolgd door de waarde van max
+# Doe een commit met de titel “input en if-statement”
+# Breidt het programma uit.
+# Bepaal (met een elif-statement) of a kleiner is dan b
+# Zo ja:
+# Ken de waarde van a toe aan een variabele Min
+# Laat het programma printen: ‘a is het kleinste getal: ’ gevolgd door de waarde van min
+# Doe een commit met de titel “elif-statement”
+# Breidt het programma uit.
+# Bepaal (met een else-statement) of a gelijk is aan b
+# Zo ja:
+# Laat het programma printen: ‘a en b zijn even groot’
+# Doe een commit met de titel “else-statement”
+# Breidt het programma uit.
+# Zorg dat in alle gevallen de variabelen Min en Max de juiste waarde krijgen
+# Laat het programma daarna printen:
+# ‘Het minimum is: ’ gevolgd door de waarde van Min
+# Het maximum is: ’ gevolgd door de waarde van Max
+# Doe een commit met de titel “Min en Max”
+
+
+a = int(input("Vul een getal in: "))
+b = int(input("Vul een getal in: "))
