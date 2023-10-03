9bc6b0f60b7c13e9295b065b5c9e87bc4de1c29a
Jacco Bos <99077364@mydavinci.nl>
2023-10-03 11:09:48 +0200

    error gefixed

--- Module_1/Deel 4 Bugs/ice_bugs.py
+++ Module_1/Deel 4 Bugs/ice_bugs.py
@@ -5,8 +5,8 @@ def convertToEuroText(amount):
 SMALL_PRICE = 1.25
 MEDIUM_PRICE = 2.10
 
-amount = input(f'Hoeveel ijsjes van {convertToEuroText(SMALL_PRICE)} wil je bestellen? ')
-amount = input(f'En hoeveel ijsjes van {convertToEuroText(MEDIUM_PRICE)} wil je bestellen? ')
-totalPrice = int(amount * SMALL_PRICE) + int(amount * MEDIUM_PRICE)
+amount1 = int(input(f'Hoeveel ijsjes van {convertToEuroText(SMALL_PRICE)} wil je bestellen? '))
+amount2 = int(input(f'En hoeveel ijsjes van {convertToEuroText(MEDIUM_PRICE)} wil je bestellen? '))
+totalPrice = float(amount1 * SMALL_PRICE) + float(amount2 * MEDIUM_PRICE)
 
-print(f'Dat is dan {totalPrice} totaal'.format(convertToEuroText(totalPrice)))
+print(f'Dat is dan {convertToEuroText(totalPrice)} totaal')
