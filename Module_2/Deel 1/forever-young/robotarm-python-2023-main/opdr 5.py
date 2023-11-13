922f0573fd57a5c6bd99c81123b8421c9b9d09e9
Jacco Bos <99077364@mydavinci.nl>
2023-11-13 13:58:46 +0100

    opdr 5

--- Module_2/Deel 1/forever-young/robotarm-python-2023-main/opdr 5.py
+++ Module_2/Deel 1/forever-young/robotarm-python-2023-main/opdr 5.py
@@ -1,6 +1,6 @@
 from RobotArm import RobotArm
 robotArm = RobotArm('exercise 5')
-robotArm.speed = 3
+robotArm.speed = 5
 robotArm.reportFlaws = False
 # Jouw python instructies zet je vanaf hier:
 #RobotArm
@@ -10,6 +10,16 @@ robotArm.reportFlaws = False
 #drop()
 #scan()
 #wait()
+for i in range(7):
+    robotArm.moveRight()
+
+for i in range(8):
+    robotArm.grab()
+    robotArm.moveRight()
+    robotArm.drop()
+    if i < 7:
+        robotArm.moveLeft()
+        robotArm.moveLeft()
 
 
 
