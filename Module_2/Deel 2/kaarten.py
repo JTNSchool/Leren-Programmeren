4d7243a3c9753d2c5e312cb6033bfb7cc3bc5a58
Jacco Bos <99077364@mydavinci.nl>
2023-11-14 10:33:10 +0100

    kaarten

---
+++ Module_2/Deel 2/kaarten.py
@@ -0,0 +1,36 @@
+import random
+
+kleuren = ("harten", "klaveren", "schoppen", "ruiten")
+nummer = list(range(2, 11))
+kaarten = nummer + ["boer", "vrouw", "heer", "aas"]
+AlleKaarten = []
+
+for kleur in kleuren:
+    for kaart in kaarten:
+        AlleKaarten.append(kleur + " " + str(kaart))
+AlleKaarten.append("joker")
+AlleKaarten.append("joker")
+
+AlleKaarten = random.sample(AlleKaarten, 54)
+for i in range(1, 8):
+    print(f"Kaart {i}: {AlleKaarten[i]}")
+    AlleKaarten.pop(i)
+
+print(f"Deck (47 kaarten): {AlleKaarten}")
+
+
+
+
+#----- KORTER
+
+# import random
+
+# kleuren, nummer, AlleKaarten = ("harten", "klaveren", "schoppen", "ruiten"), list(range(2, 11)), []
+# kaarten = nummer + ["boer", "vrouw", "heer", "aas"]
+
+# for kleur in kleuren:
+#     for kaart in kaarten: AlleKaarten.append(kleur + " " + str(kaart))
+# AlleKaarten.append("joker"); AlleKaarten.append("joker"); AlleKaarten = random.sample(AlleKaarten, 54)
+
+# for i in range(1, 8): print(f"Kaart {i}: {AlleKaarten[i]}"); AlleKaarten.pop(i)
+# print(f"Deck (47 kaarten): {AlleKaarten}")
