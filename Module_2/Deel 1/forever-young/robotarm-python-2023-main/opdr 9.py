f2671beea0cbe5121c999b8d97df69a55f991243
Jacco Bos <99077364@mydavinci.nl>
2023-11-15 10:16:51 +0100

    opdr 9

--- Module_2/Deel 1/forever-young/robotarm-python-2023-main/opdr 9.py
+++ Module_2/Deel 1/forever-young/robotarm-python-2023-main/opdr 9.py
@@ -1,6 +1,6 @@
 from RobotArm import RobotArm
-robotArm = RobotArm('exercise 6')
-robotArm.speed = 3
+robotArm = RobotArm('exercise 9')
+robotArm.speed = 5
 robotArm.reportFlaws = False
 # Jouw python instructies zet je vanaf hier:
 #RobotArm
@@ -10,16 +10,20 @@ robotArm.reportFlaws = False
 #drop()
 #scan()
 #wait()
-robotArm.moveRight()
-for i in range(3):
-    robotArm.grab()
-    robotArm.moveLeft()
-    robotArm.drop()
+
+block = 1
+
+for i in range(4):
+    for i in range(block):
+        robotArm.grab()
+        for i in range(5):
+            robotArm.moveRight()
+        robotArm.drop()
+        for i in range(5):
+            robotArm.moveLeft()
     robotArm.moveRight()
-    robotArm.grab()
-    robotArm.moveRight()
-    robotArm.drop()
-    robotArm.moveLeft()
+    block += 1
+
 
 # Na jouw code wachten tot het sluiten van de window:
 robotArm.wait()
