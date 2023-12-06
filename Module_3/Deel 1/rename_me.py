4bbf4f031ebf9f4a6dd49067f45b921678681fb3
Jacco Bos <99077364@mydavinci.nl>
2023-12-06 10:08:37 +0100

    rename gemaakt

--- Module_3/Deel 1/rename_me.py
+++ Module_3/Deel 1/rename_me.py
@@ -1,31 +1,27 @@
-def quantum_broodrooster(stellar_broccoli:int) -> bool: #Kan delen door 2 true anders false
-    return stellar_broccoli % 2 == 0
+def KanDelenDoorTwee(Getal:int) -> bool: #Kan delen door 2 true anders false
+    return Getal % 2 == 0
 
-def chaos_papegaai(fantasie_platypus:str) -> str: #reversed zin 
-    betoverde_druif = fantasie_platypus.split()
-    doldwaze_broccoli = betoverde_druif[::-1]
-    tijdmachine_pannenkoekenmix = ' '.join(doldwaze_broccoli)
-    return tijdmachine_pannenkoekenmix
+def DraaiZinOm(Zin:str) -> str: #reversed zin 
+    GesplitZin = Zin.split()
+    ZinOmdraaier = GesplitZin[::-1]
+    OmgeDraaidZin = ' '.join(ZinOmdraaier)
+    return OmgeDraaidZin
 
-def kosmische_koekjesmix(galactische_snoepjes:str) -> int: #Aantal letters
-    planetair_taartje = set(galactische_snoepjes)
-    whatchamacallit = len(planetair_taartje)
-    return whatchamacallit
+def AantalLetters(Zin:str) -> int: #Aantal letters
+    LetterLijst = set(Zin)
+    AaatalLettersInZin = len(LetterLijst)
+    return AaatalLettersInZin
 
-def ruimte_hamsterwiel(planetair_taartje:str) -> float: #gemiddelde letters per woord
-    wobbelwobbel = planetair_taartje.split()
-    
-    blork = 0
-    for snorkelwagen in wobbelwobbel:
-        blork += len(snorkelwagen)
+def GemiddeldeLetters(Zin:str) -> float: #gemiddelde letters per woord
+    GesplitteZin = Zin.split()
+    Letters = 0
+    for word in GesplitteZin:
+        Letters += len(word)
 
-    bizarro_matrix = blork / len(wobbelwobbel)
-    return bizarro_matrix
+    GemiddeldeLetterPerWoord = Letters / len(Zin)
+    return GemiddeldeLetterPerWoord
 
-def spaghetti_spaceship(infinity_pizza:int, parallelle_tosti:int=10) -> None: #Keer som als arg-2 niet er is dan zet het naar 10
-    for zwabber_krakeling in range(1, parallelle_tosti+1):
-        laser_sandwich = zwabber_krakeling * infinity_pizza
-        print(f'{zwabber_krakeling} x {infinity_pizza} = {laser_sandwich}')
-
-a = spaghetti_spaceship(6)
-print(a)
+def KeerSom(Getal1:int, Getal2:int=10) -> None: #Keer som als arg-2 niet er is dan zet het naar 10
+    for Getal in range(1, Getal2+1):
+        Antwoord = Getal * Getal1
+        print(f'{Getal} x {Getal1} = {Antwoord}')
