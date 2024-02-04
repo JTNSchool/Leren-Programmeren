0d9b9b74d518619347bd8448b116c3e92f165e6c
Jacco Bos <99077364@mydavinci.nl>
2024-02-04 16:22:05 +0100

    pizza calcc :)

---
+++ Module_3/Deel 4/PizzaCalc.py
@@ -0,0 +1,47 @@
+SOORTEN = ["Small", "Medium", "Large"]
+PRIJZEN = {
+    "Small": 6.50,
+    "Medium": 9.30,
+    "Large": 11.10,
+}
+
+#Check of er genoeg prijzen zijn in verglijking met soorten
+for soort in SOORTEN:
+    if soort not in PRIJZEN  or len(PRIJZEN) != len(SOORTEN):
+        exit("ERROR: DE SOORTEN EN PRIJZEN ZIJN NIET HET ZELFDE")
+
+def Input_Int(txt):
+    while True:
+        antwoord = input(txt + " ")
+        try:
+            antwoord = int(antwoord)
+            return antwoord
+        except ValueError:
+            print(f"{antwoord} is geen heel getal")
+
+#Aantal pizzas
+AantalPizzas = {}
+for grote in SOORTEN:
+    amount = Input_Int(f"Hoeveel {grote} pizza's wilt u?")
+    if amount >= 1:
+        AantalPizzas.update({grote: amount})
+
+#kosten berekenen
+Kosten = {}
+for pizza in AantalPizzas:
+    cost = round(AantalPizzas[pizza] * PRIJZEN[pizza], 2)
+    try:
+        cost = int(cost)
+    except ValueError:
+        pass
+    Kosten.update({pizza: cost})
+
+#Bonnentje
+print('************** KASSA BON *************')
+totaal = 0
+for pizza in Kosten:
+    print(f"Pizza's {pizza}:    {AantalPizzas[pizza]} x {PRIJZEN[pizza]} = {Kosten[pizza]}")
+    totaal += Kosten[pizza]
+
+print('--------------------------------------')
+print(f'Te betalen:                   €{totaal}')
